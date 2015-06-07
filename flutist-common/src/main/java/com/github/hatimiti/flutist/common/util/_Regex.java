3b3d7bf166043887ffa57d2e327345dde742aeb1
hatimiti <hatti33@gmail.com>
2015-06-07 11:05:33 +0900

    正規表現utilにメソッド追加

--- flutist-common/src/main/java/com/github/hatimiti/flutist/common/util/_Regex.java
+++ flutist-common/src/main/java/com/github/hatimiti/flutist/common/util/_Regex.java
@@ -1,12 +1,13 @@
 package com.github.hatimiti.flutist.common.util;
 
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 /**
  * 正規表現ユーティリティクラス
- * @author m-kakimi
+ * @author hatimiti
  */
 public final class _Regex {
 
@@ -18,15 +19,30 @@ public final class _Regex {
 	public static String[] ESCAPE_CHARS = {
 		"\\", "*", "+", ".", "?", "{", "}", "(", ")", "[", "]", "^", "$", "-", "|", "/"
 	};
-	
+
 	public static Pattern ESCAPE_PATTERN = Pattern.compile("(["
 			+ Stream.of(ESCAPE_CHARS)
 				.map(it -> "\\" + it)
 				.collect(Collectors.joining())
 		+ "])");
-	
-	
+
+
 	public static String escape(String value) {
 		return ESCAPE_PATTERN.matcher(value).replaceAll("\\\\$1");
+	}
+
+	public static String replaceAll(String target, String regex, String replacement) {
+		if (target == null) {
+			return null;
+		}
+		Matcher matcher = Pattern.compile(regex).matcher(target);
+		StringBuffer sb = new StringBuffer();
+		while (matcher.find()) {
+			// 一致したグループは matcher.group(n) で取得できる。
+			// ここで replacement を加工する。
+			matcher.appendReplacement(sb, replacement);
+		}
+		matcher.appendTail(sb);
+		return sb.toString();
 	}
 }
